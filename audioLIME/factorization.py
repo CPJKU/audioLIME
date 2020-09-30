9d94605f4ad044488ecd0e8fbc8bf636fecfd843
Verena Haunschmid <veroamilbe@gmail.com>
2020-09-30 08:02:32 +0200

    Prepare storing of sources

--- audioLIME/factorization.py
+++ audioLIME/factorization.py
@@ -4,6 +4,7 @@ import os
 import librosa
 from audioLIME.factorization_base import Factorization
 from audioLIME.data_provider import RawAudioProvider
+import pickle
 
 try:
     import torch
@@ -106,6 +107,13 @@ class DataBasedFactorization(Factorization):
         self.prepare_components(start_sample, y_length)
 
 
+def separate(separator, waveform, target_sr, spleeter_sr):
+    waveform = np.expand_dims(waveform, axis=0)
+    waveform = librosa.resample(waveform, target_sr, spleeter_sr)
+    waveform = np.swapaxes(waveform, 0, 1)
+    prediction = separator.separate(waveform)
+    return prediction
+
 class SpleeterFactorization(DataBasedFactorization):
     def __init__(self, data_provider, n_temporal_segments, composition_fn, model_name, target_sr=16000):
         assert isinstance(data_provider, RawAudioProvider)  # TODO: nicer check
@@ -123,10 +131,7 @@ class SpleeterFactorization(DataBasedFactorization):
 
         # Perform the separation:
         waveform = self.data_provider.get_mix()
-        waveform = np.expand_dims(waveform, axis=0)
-        waveform = librosa.resample(waveform, self.target_sr, spleeter_sr)
-        waveform = np.swapaxes(waveform, 0, 1)
-        prediction = separator.separate(waveform)
+        prediction = separate(separator, waveform, self.target_sr, spleeter_sr)
 
         self.original_components = [
             librosa.resample(np.mean(prediction[key], axis=1), spleeter_sr, self.target_sr) for
@@ -143,16 +148,23 @@ class SpleeterPrecomputedFactorization(DataBasedFactorization):
         sample_name = os.path.basename(data_provider.get_audio_path().replace(".mp3", ""))
         self.sources_path = os.path.join(spleeter_sources_path,
                                          model_name.replace("spleeter:", ""), sample_name)
-        print(self.sources_path)
+        # print(self.sources_path)
+
         super().__init__(data_provider, n_temporal_segments, composition_fn)
 
     def initialize_components(self):
         spleeter_sr = 44100
 
-        prediction = {}
-        for source_file in os.listdir(self.sources_path):
-            instr = source_file.replace(".wav", "")
-            prediction[instr], _ = librosa.load(os.path.join(self.sources_path, source_file), sr=spleeter_sr, mono=True)
+        prediction_path = os.path.join(self.sources_path, "prediction.pt")
+        if not os.path.exists(prediction_path):
+            separator = Separator(self.model_name, multiprocess=False)
+            waveform = self.data_provider.get_mix()
+            prediction = separate(separator, waveform, self.target_sr, spleeter_sr)
+            pickle.dump(prediction, open(prediction_path, "wb"))
+        else:
+            print("Loading", prediction_path)
+
+        prediction = pickle.load(open(prediction_path, "rb"))
 
         self.original_components = [
             librosa.resample(np.mean(prediction[key], axis=1), spleeter_sr, self.target_sr) for
